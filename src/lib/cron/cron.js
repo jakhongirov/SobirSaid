490eeac06142d8ff073d57520a1d4134abd38937
jakhongirov <johongirovdiyorbek08@gmail.com>
2025-01-21 19:28:58 +0800

    add

--- src/lib/cron/cron.js
+++ src/lib/cron/cron.js
@@ -10,6 +10,7 @@ const sendText = async () => {
    for (const user of usersList) {
       const text = `m=66ba0b7a9412202b2cc2e5aa;ac.user_id=${user?.chat_id};ac.tarif=Spreylarsiz%20hayot;ac.ilova=SobirSaid;a=9700000`;
       const base64Encoded = btoa(text);
+      const content = localText.everyDay?.replace(/%name%/g, user?.name)
       bot.sendMessage(user?.chat_id, localText.everyDay, {
          reply_markup: {
             inline_keyboard: [
